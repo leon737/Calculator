c4b47c3d9e3c4804d40b23a818eaf943d71705ac
Leonid Gordo <leonardpt@gmail.com>
2019-01-31 00:28:25 +0300

    mousedown and up events

--- src/components/Key.vue
+++ src/components/Key.vue
@@ -1,19 +1,26 @@
 <template>
-    <div :class="buttonClass" @click="click">
+    <div :class="buttonClass" @click="click" @mousedown="mousedown" @mouseup="mouseup">
         {{button.symbol}}
     </div>
 </template>
 <script>
 export default {
+    data() {
+        return {
+            down: false
+        }
+    },
     props: {
         button: Object
     },
     computed: {
         buttonClass() {
-            return `button ${this.button.spanRows ? 'button-2-rows' : ''} ${this.button.color ? this.button.color: ''}`
+            return `button ${this.button.spanRows ? 'button-2-rows' : ''} ${this.button.color ? this.button.color: 'gray'}${this.down ? '-down': ''}`
         }
     },
-    methods: {        
+    methods: {   
+        mousedown() { this.down = true },
+        mouseup() { this.down = false },
         click() {
             switch(this.button.action) {
                 case 'digit':
@@ -68,6 +75,18 @@ export default {
 
     .blue {
         background-color: #36369c;
+    }
+
+    .red-down {
+        background-color: #831515;
+    }
+
+    .blue-down {
+        background-color: #1d1d5a;
+    }
+
+    .gray-down {
+        background-color: #817f7f;
     }
 
 </style>
